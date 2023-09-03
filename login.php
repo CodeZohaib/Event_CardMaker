8f8908c950603660eddb64901c9cd7e1651b3531
CodeZohaib <webdeveloper935@gmail.com>
2023-09-03 11:03:34 +0500

    Login page update

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
                 <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
                     <div class="card col-lg-4 mx-auto">
                         <div class="card-body px-5 py-5">
-                            <h3 class="card-title text-left mb-3">Register</h3>
+                            <h3 class="card-title text-left mb-3">Login</h3>
                             <form action="files/requestPage.php" method="post" class="form">
 
                                 <div class="form-group">
